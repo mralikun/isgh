396de13c09284065e390b29c5948d7bed256df71
Ahmed Ali <ahmed2dawood3@yahoo.com>
2015-10-20 05:34:32 -0700

    seeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,22 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Model::unguard();
+        $this->call('RolesTableSeeder');
 
-		// $this->call('UserTableSeeder');
+        $this->command->info('Roles table seeded!');
 	}
+
+}
+
+class RolesTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('roles')->delete();
+
+        \App\Roles::create(['role_name' => 'admin' , "id"=>1]);
+        \App\Roles::create(['role_name' => 'khateeb' , "id"=>2]);
+        \App\Roles::create(['role_name' => 'ad' , "id"=>3]);
+    }
 
 }
