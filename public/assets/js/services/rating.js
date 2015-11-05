5ed3ac4fb2ae545152a90f1bbc7d6c1ebf0afa7a
= <=>
2015-11-05 15:48:02 +0200

    fixed empty id value in rating request

--- public/assets/js/services/rating.js
+++ public/assets/js/services/rating.js
@@ -4,6 +4,7 @@ var model = {
 
 var view = {
     render: function(dataArray){
+        console.log(dataArray[0].id);
         for(var i = 0; i < dataArray.length; i++){
             $("#allUsers").append("<div class='row rating-row'><div class='col-sm-7 sol-md-7 col-lg-7'><div class='row'><div class='col-sm-4 col-md-4 col-lg-4'><img class='img-responsive rating-img' src='/images/khateeb_pictures/"+dataArray[i].picture_url+"'></div><div class='col-sm-8 col-md-8 col-lg-8'><h3 class='rating-name'>"+dataArray[i].name+"</h3></div></div></div><div class='col-sm-5 col-md-5 col-lg-5 stars' id='"+dataArray[i].id+"'></div></div>");
         }
@@ -19,20 +20,20 @@ $(document).ready(function(){
             data: {id: _id , _token: $("input[name='_token']").val()},
             dataType: "json",
             success: function(resp){
-                console.log(resp);
                 view.render(resp);
                 $(".stars").raty({
                     number: 7,
                     starType: "i",
                     cancel: true,
                     click: function(score , event){
+                        var _ID = parseInt($(event.target).parents(".stars").attr("id"));
                         $.ajax({
                             type: "POST",
                             url: "/user/rate",
                             dataType: "json",
-                            data: {id: event.target.id , rate: (!!score) ? score : 0 , _token: $("input[name='_token']").val()},
+                            data: {id: _ID , rate: (!!score) ? score : 0 , _token: $("input[name='_token']").val()},
                             success: function(resp){
-                                console.log(resp);
+                                
                             },
                             error: function(err){
                                 ISGH.alertBox.init("Something went wrong trying to sumbit your rates, Please refresh and try again" , false);
